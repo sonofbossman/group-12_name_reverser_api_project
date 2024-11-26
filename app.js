57586b4b77bc0cfd43a7ca86a66a3cf2084495da
sonofbossman <akingbadeeniola452@gmail.com>
2024-11-26 16:25:57 +0100

    Set-up the app.js file.

---
+++ app.js
@@ -0,0 +1,17 @@
+// import the dependencies
+import express from 'express'
+import { router } from './routes/route.js'
+
+const app = express()
+const PORT = 2200
+
+// set-up middleware for handling json payloads and form data
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) 
+
+app.use('/', router)
+
+// setup listener 
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on port ${PORT}...`)
+})
