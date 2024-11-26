a8a83fc476a3c3e236f296cb55380000a49441d4
sonofbossman <akingbadeeniola452@gmail.com>
2024-11-26 16:22:17 +0100

    Added the reverse logic in controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -9,4 +9,22 @@ function submitName(req, res) {
         res.status(400).json({error: "Name query parameter is invalid"})
        }
     }
+
+    res.json(reverseNameFunc(name))
+}
+
+function reverseNameFunc(name){
+
+  /*
+      To reverse a name, we first use the split() function to convert the string into an array,
+      then apply the reverse() function to reverse the array.
+      Finally, use the join() function to convert the reversed array back into a string.
+  */
+
+  const reversed_name = name.split('').reverse().join('')
+  console.log(reversed_name) 
+  return {
+      original: name,
+      reversed: reversed_name
+  }    
 }
